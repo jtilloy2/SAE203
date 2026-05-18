801fa7bad0b7e96656b5e2b87f6f592c3fc2962d
elouanlejoly <elouan.lejoly@gmail.com>
2026-05-18 15:44:06 +0200

    Update index.php
    Ajout des 2 nouveaux liens

--- intranet/index.php
+++ intranet/index.php
@@ -18,6 +18,7 @@ if (!isset($_SESSION['user'])) {
             <h1 class="text-center">Bienvenue, <?= htmlspecialchars($_SESSION['user']['nom']) ?> !</h1>
             <p class="text-center text-muted">Rôle : <?= htmlspecialchars($_SESSION['user']['role']) ?></p>
             <hr>
+            
             <div class="row g-3">
                 <div class="col-md-6">
                     <a href="clients.php" class="btn btn-primary btn-lg w-100 p-4">👥 Annuaire Clients</a>
@@ -25,7 +26,15 @@ if (!isset($_SESSION['user'])) {
                 <div class="col-md-6">
                     <a href="partenaires_admin.php" class="btn btn-info btn-lg w-100 p-4 text-white">🤝 Partenaires</a>
                 </div>
+                
+                <div class="col-md-6">
+                    <a href="annuaire.php" class="btn btn-success btn-lg w-100 p-4">🧑‍💼 Annuaire Salariés</a>
+                </div>
+                <div class="col-md-6">
+                    <a href="fichiers.php" class="btn btn-warning btn-lg w-100 p-4 text-dark">📁 Fichiers Partagés</a>
+                </div>
             </div>
+
             <div class="mt-4 text-center">
                 <a href="logout.php" class="btn btn-link text-danger">Se déconnecter</a>
             </div>
